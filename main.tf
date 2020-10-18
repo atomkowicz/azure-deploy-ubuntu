bc6167eae2c5b74b5711565484aa85233c37695d
Agnieszka Tomkowicz <agnieszka.tomkowicz@fortedigital.no>
2020-10-18 11:03:08 +0200

    Add Network security group, rules

--- main.tf
+++ main.tf
@@ -28,6 +28,41 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.0.0/24"]
 }
 
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.prefix}-network-security-group"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = local.tags
+}
+
+resource "azurerm_network_security_rule" "allow-inbound-vnet" {
+  name                        = "AllowInboundVnet"
+  priority                    = 190
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "TCP"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "10.0.0.0/16"
+  destination_address_prefix  = "10.0.0.0/16"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
+
+resource "azurerm_network_security_rule" "deny-inbound-traffic" {
+  name                        = "DenyInboundTraffic"
+  priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
+
 resource "azurerm_public_ip" "main" {
   name                = "${var.prefix}-public-ip"
   resource_group_name = azurerm_resource_group.main.name
